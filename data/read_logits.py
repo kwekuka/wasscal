d934efd50e85783521af5fe01638b175bee341f7
kwekuka <mark_kwegyir-aggrey@brown.edu>
2023-05-03 14:25:50 -0400

    make loader into a class

--- data/read_logits.py
+++ data/read_logits.py
@@ -1,14 +1,33 @@
-import pickle 
 import os
+import pickle
+import numpy as np
 
-if __name__== "__main__":
-    abspath = os.path.abspath(__file__)
-    dname = os.path.dirname(abspath)
-    os.chdir(dname)
-    with (open("logits_c10_pretrained.p", "rb")) as openfile:
-        x = pickle.load(openfile)
+class Loader():
+    def __init__(self, dataset):
+        abspath = os.path.abspath(__file__)
+        dname = os.path.dirname(abspath)
+        os.chdir(dname)
 
-    logits = x[0] #shape (#samples, #classes) (10000,10)
-    labels = x[1] # shape (#samples) (10000,)
-    print(logits.shape)
-    print(labels.shape)
+        if dataset == "CIFAR10":
+            with (open("logits_c10_pretrained.p", "rb")) as openfile:
+                x = pickle.load(openfile)
+        elif dataset == "CIFAR100":
+            with (open("logits_c100_pretrained.p.p", "rb")) as openfile:
+                x = pickle.load(openfile)
+
+        #
+        self.vector = np.exp(x[0])
+        self.labels = x[1]
+
+
+# if __name__== "__main__":
+#     abspath = os.path.abspath(__file__)
+#     dname = os.path.dirname(abspath)
+#     os.chdir(dname)
+#     with (open("logits_c10_pretrained.p", "rb")) as openfile:
+#         x = pickle.load(openfile)
+#
+#     logits = x[0] #shape (#samples, #classes) (10000,10)
+#     labels = x[1] # shape (#samples) (10000,)
+#     print(logits.shape)
+#     print(labels.shape)
